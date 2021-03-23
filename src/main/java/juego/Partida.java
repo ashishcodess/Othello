5badecf4e3767f65b5aca8f9513f2aec4417726c
sergibosquet <sergi.bosquet@estudiantat.upc.edu>
2021-03-23 11:00:25 +0100

    Agregado el setPartida en la clase Partida

--- src/main/java/juego/Partida.java
+++ src/main/java/juego/Partida.java
@@ -16,20 +16,33 @@ public class Partida {
 
     public String getModoDeJuegoPartida(int idPartida) {
         if (this.id == idPartida) return this.modoDeJuego;
+        return null;
     }
 
     public String getReglasPartida(int idPartida) {
         if (this.id == idPartida) return this.reglas;
+        return null;
     }
 
     public int getTurnoPartida(int idPartida) {
         if (this.id == idPartida) return this.turno;
+        return idPartida;
     }
 
     public String getJugador1Partida(int idPartida) {
         if (this.id == idPartida) return this.jugador1;
+        return null;
     }
     public String getJugador2Partida(int idPartida) {
         if (this.id == idPartida) return this.jugador2;
+        return null;
+    }
+    //Configuración de los parámetros de una partida
+    public void setPartida(String modoJuego, String r, int turn, String j1, String j2){
+        modoDeJuego = modoJuego;
+        reglas = r;
+        turno = turn;
+        jugador1 = j1;
+        jugador2 = j2;
     }
 }
