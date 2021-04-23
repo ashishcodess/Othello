83e5acd030af7cb7b55c9ae334e5e3ab4c6a4829
sergi <sergi.cassanmagnago@gmail.com>
2021-04-23 20:02:52 +0200

    .

--- src/main/java/Dominio/SuccessorFunction.java
+++ src/main/java/Dominio/SuccessorFunction.java
@@ -1,4 +1,16 @@
 package Dominio;
 
 public class SuccessorFunction { //necessaria para implementar la IA
+    Tablero[] succesores;
+    Casilla[] disponibles_blancas;
+    Casilla[] disponibles_negras;
+
+    /**
+     *
+     * @param t es el tablero a partir del cual generamos su lista de estados succesores
+     * @return
+     */
+    public Tablero[] succesorFunction(Tablero t){
+        return succesores;
+    }
 }
