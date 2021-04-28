e43797e338a9c4984df9e4b37877b9044ab61123
ashish.kshetri <ashish.kshetri@estudiantat.upc.edu>
2021-04-28 22:26:50 +0200

    Version definitivo

--- src/main/java/drivers/DriverTablero.java
+++ src/main/java/drivers/DriverTablero.java
@@ -18,6 +18,8 @@ import java.io.IOException;
 public class DriverTablero {
     static Scanner scan = new Scanner(System.in);
 
+    /** test_tablero para ver que el tablero inicial imprime bien (clase hijo)
+     * */
     public static void test_tablero(){
         Tablero T = new Tablero();
         for (int i = 0; i < 8; ++i) {
@@ -30,57 +32,11 @@ public class DriverTablero {
         System.out.println();
 
     }
-    public static void test_graph_v(){
-
-        Tablero T = new Tablero();
-
-        T.setCasilla_tipo(0,0 , 2);
-        T.setCasilla_tipo(5,5 , 1);
-        int[][] graph_v = new int[8][8];
-        for(int i = 0 ; i < 8 ; ++i){
-            for (int j = 0 ; j < 8 ; ++j){
-                graph_v[i][j]= T.getCasilla_tipo(i,j);
-            }
-        }
-        for (int i = 0; i < 8; ++i) {
-            String sbuff = new String();
-            for (int j = 0; j < 8; ++j) {
-                sbuff = sbuff + graph_v[i][j];
-            }
-            System.out.println(sbuff);
-        }
-        System.out.println();
-
-
-    }
+    /** print_dispnibles para ver cuales son los primeros casillas disponibles cuando empezamos la partida
+     * */
     public static void print_disponibles() {
-        /*Position pos = new Position(1 , 1);
-        Queue<Position> q = new LinkedList<>();
-        q.add(pos);
-        Position current_pos = q.element();
-        int x = current_pos.getX();
-        int y = current_pos.getY();
-        System.out.println(x);*/
 
-       /*Tablero T = new Tablero();
-
-
-        T.setCasilla_tipo(0,0 , 1);
-        T.setCasilla_tipo(5,5 , 1);
-        T.setCasilla_tipo(5,5 , 1); // esto no debería ser añadido
-        Set<Position> disp = T.getCasillasDisponibles();
-        Position p1 = new Position(3 , 4);
-        Position p2 =new Position(4,4);
-        disp.add(p1);
-        disp.add(p2);
-        disp.remove(p1);
-        Object[] arr = disp.toArray();
-        for (int i = 0 ; i < arr.length ; ++i){
-            Position P = (Position)arr[i];
-            System.out.println(P.getX());
-            System.out.println(P.getY());
-        }
-*/      int turno = 6;
+        int turno = 6;
         Tablero T = new Tablero();
 
 
@@ -116,25 +72,173 @@ public class DriverTablero {
         }
 
     }
+    /** print_modificadas es una simulacion pequena hasta 5 turnos(3 de negras y 2 de blancas)
+     * Aqui para cada turno primero va a imprimir posiciones de casillas disponible, despues tableo actual donde : disponibles = 1, negras = 2, blancas= 3 y vacios = 0.
+     * Cuando ya vemos cuales son los disponibles vamos a escoger un posicion de los disponibles y actualizamos el tablero.
+     * Al final para cada turno imprimimos tablero actualizo es decir los disponibles pasan a ser 0 para que cuando toca al siguente persona vuelve a calcular cuales son los disponibles para el.
+     * Este proceso se repite para 5 turnos simulados.
+     * */
     public static void print_modificadas() {
-        int turno = 6;
+        int turno = 0;
         Tablero T = new Tablero();
 
         T.calcularCasillasDisponiblesHorizontal(turno);
         T.calcularCasillasDisponiblesVertical(turno);
         T.calcularCasillasDisponiblesDiagonales(turno);
+
         Set<Position> disp = T.getCasillasDisponibles();
         Object[] arr = disp.toArray();
+        System.out.println("Imprimir todas las pos disponibles:");
         for (int i = 0 ; i < arr.length ; ++i){
             Position P = (Position)arr[i];
             System.out.println("Dx:" + P.getX() + "Dy:" + P.getY());
         }
 
-        T.actualizarTablero(2 ,3 , turno);
-        /*T.actualizarTablero(2 ,2 , 1);
-        T.actualizarTablero(3 ,2 , turno);
-        T.actualizarTablero(4 ,2 , 1);*/
+        System.out.println("Imprimir Tablero para turno 1(negro):");
+        for (int i = 0; i < 8; ++i) {
+            String sbuff = new String();
+            for (int j = 0; j < 8; ++j) {
+                sbuff = sbuff + T.getCasilla_tipo(i,j);
+            }
+            System.out.println(sbuff);
+        }
+        System.out.println();
 
+        T.actualizarTablero(2 ,3 , turno);
+
+        System.out.println("Imprimir tablero actualizado");
+        for (int i = 0; i < 8; ++i) {
+            String sbuff = new String();
+            for (int j = 0; j < 8; ++j) {
+                sbuff = sbuff + T.getCasilla_tipo(i,j);
+            }
+            System.out.println(sbuff);
+        }
+        System.out.println();
+
+        T.calcularCasillasDisponiblesHorizontal(1);
+        T.calcularCasillasDisponiblesVertical(1);
+        T.calcularCasillasDisponiblesDiagonales(1);
+        disp = T.getCasillasDisponibles();
+        arr = disp.toArray();
+
+        System.out.println("Imprimir todas las pos disponibles:");
+        for (int i = 0 ; i < arr.length ; ++i){
+            Position P = (Position)arr[i];
+            System.out.println("Dx:" + P.getX() + "Dy:" + P.getY());
+        }
+
+        System.out.println("Imprimir Tablero para turno 2(blanco):");
+        for (int i = 0; i < 8; ++i) {
+            String sbuff = new String();
+            for (int j = 0; j < 8; ++j) {
+                sbuff = sbuff + T.getCasilla_tipo(i,j);
+            }
+            System.out.println(sbuff);
+        }
+        System.out.println();
+
+        T.actualizarTablero(2 ,2 , 1);
+
+        System.out.println("Imprimir tablero actualizado");
+        for (int i = 0; i < 8; ++i) {
+            String sbuff = new String();
+            for (int j = 0; j < 8; ++j) {
+                sbuff = sbuff + T.getCasilla_tipo(i,j);
+            }
+            System.out.println(sbuff);
+        }
+        System.out.println();
+
+
+        T.calcularCasillasDisponiblesHorizontal(turno);
+        T.calcularCasillasDisponiblesVertical(turno);
+        T.calcularCasillasDisponiblesDiagonales(turno);
+        disp = T.getCasillasDisponibles();
+        arr = disp.toArray();
+
+        System.out.println("Imprimir todas las pos disponibles:");
+        for (int i = 0 ; i < arr.length ; ++i){
+            Position P = (Position)arr[i];
+            System.out.println("Dx:" + P.getX() + "Dy:" + P.getY());
+        }
+        System.out.println("Imprimir Tablero para turno 3(negro):");
+        for (int i = 0; i < 8; ++i) {
+            String sbuff = new String();
+            for (int j = 0; j < 8; ++j) {
+                sbuff = sbuff + T.getCasilla_tipo(i,j);
+            }
+            System.out.println(sbuff);
+        }
+        System.out.println();
+
+        T.actualizarTablero(3 ,2 , turno);
+        System.out.println("Imprimir tablero actualizado");
+        for (int i = 0; i < 8; ++i) {
+            String sbuff = new String();
+            for (int j = 0; j < 8; ++j) {
+                sbuff = sbuff + T.getCasilla_tipo(i,j);
+            }
+            System.out.println(sbuff);
+        }
+        System.out.println();
+
+
+        T.calcularCasillasDisponiblesHorizontal(1);
+        T.calcularCasillasDisponiblesVertical(1);
+        T.calcularCasillasDisponiblesDiagonales(1);
+        disp = T.getCasillasDisponibles();
+        arr = disp.toArray();
+        System.out.println("Imprimir todas las pos disponibles:");
+        for (int i = 0 ; i < arr.length ; ++i){
+            Position P = (Position)arr[i];
+            System.out.println("Dx:" + P.getX() + "Dy:" + P.getY());
+        }
+        System.out.println("Imprimir Tablero para turno 4(blanco):");
+        for (int i = 0; i < 8; ++i) {
+            String sbuff = new String();
+            for (int j = 0; j < 8; ++j) {
+                sbuff = sbuff + T.getCasilla_tipo(i,j);
+            }
+            System.out.println(sbuff);
+        }
+        System.out.println();
+
+        T.actualizarTablero(4 ,2 , 1);
+        System.out.println("Imprimir tablero actualizado");
+        for (int i = 0; i < 8; ++i) {
+            String sbuff = new String();
+            for (int j = 0; j < 8; ++j) {
+                sbuff = sbuff + T.getCasilla_tipo(i,j);
+            }
+            System.out.println(sbuff);
+        }
+        System.out.println();
+
+
+        T.calcularCasillasDisponiblesHorizontal(turno);
+        T.calcularCasillasDisponiblesVertical(turno);
+        T.calcularCasillasDisponiblesDiagonales(turno);
+        disp = T.getCasillasDisponibles();
+        arr = disp.toArray();
+
+        System.out.println("Imprimir todas las pos disponibles:");
+        for (int i = 0 ; i < arr.length ; ++i){
+            Position P = (Position)arr[i];
+            System.out.println("Dx:" + P.getX() + "Dy:" + P.getY());
+        }
+        System.out.println("Imprimir Tablero para turno 5(negro):");
+        for (int i = 0; i < 8; ++i) {
+            String sbuff = new String();
+            for (int j = 0; j < 8; ++j) {
+                sbuff = sbuff + T.getCasilla_tipo(i,j);
+            }
+            System.out.println(sbuff);
+        }
+        System.out.println();
+
+        T.actualizarTablero(1 ,1 , turno);
+        System.out.println("Imprimir tablero actualizado");
         for (int i = 0; i < 8; ++i) {
             String sbuff = new String();
             for (int j = 0; j < 8; ++j) {
@@ -144,7 +248,11 @@ public class DriverTablero {
         }
         System.out.println();
     }
-
+    /**
+     * funcion main (para poder realizar las pruebas)
+     * @param args (argumentos)
+     * @throws MyException heredado de el resto de funciones
+     * */
     public static void main(String[] args) throws IOException, MyException {
         boolean b = true;
         while (b) {
@@ -170,9 +278,6 @@ public class DriverTablero {
                 case 1:
                     test_tablero();
                     break;
-                case 2:
-                    test_graph_v();
-                    break;
                 case 6:
                     print_disponibles();
                     break;
@@ -187,18 +292,3 @@ public class DriverTablero {
     }
 
 }
-
-
-
-    /*Set<Position> blancas = T.getCasillasBlancas();
-    Object[] arr1 = blancas.toArray();
-        for (int i = 0 ; i < arr1.length ; ++i){
-        Position P = (Position)arr1[i];
-        System.out.println("Bx:" + P.getX() + "By:" + P.getY());
-        }
-        Set<Position> negras = T.getCasillasNegras();
-        Object[] arr2 = negras.toArray();
-        for (int i = 0 ; i < arr2.length ; ++i){
-        Position P = (Position)arr2[i];
-        System.out.println("Nx:" + P.getX() + "Ny:" + P.getY());
-        }*/
