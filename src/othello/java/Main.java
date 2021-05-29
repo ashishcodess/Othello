e3ddfea0d4f85163c06fb6376638fb94840d4d79
SergioAguado <sergio.aguado.cobos@est.fib.upc.edu>
2021-05-29 15:34:35 +0200

    cambios al MakeFile (aun no funciona .jar si se crea desde el MakeFile)

--- src/othello/java/Main.java
+++ src/othello/java/Main.java
@@ -5,7 +5,6 @@ import javax.swing.*;
 /**Funcion Main del proyecto Othello*/
 public class Main {
     public static void main (String[] args) {
-        System.out.println(System.getProperty("java.class.path"));
         SwingUtilities.invokeLater (
                 () -> {
                     CtrlPresentacion ctrlPresentacion = new CtrlPresentacion();
